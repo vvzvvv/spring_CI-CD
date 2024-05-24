0a4865f6944b82c7bb235586447698cba40ac6a2
choiyoorim <yulim3839@naver.com>
2024-05-24 12:12:30 +0900

    doc: 📝 constants 폴더 추가

---
+++ constants/responseMessage.js
@@ -0,0 +1,55 @@
+module.exports = {
+    NULL_VALUE: '필요한 값이 없습니다',
+    OUT_OF_VALUE: '파라미터 값이 잘못되었습니다',
+    TOO_MUCH_LONG_VALUE: '파라미터 값이 너무 깁니다',
+  
+    // 회원가입
+    CREATED_USER: '회원 가입 성공',
+    DELETE_USER: '회원 탈퇴 성공',
+    ALREADY_EMAIL: '이미 사용중인 이메일입니다.',
+    DIFFRERENT_PASSWORD: '비밀번호와 비밀번호 확인이 일치하지 않습니다',
+    SIGNUP_OK: '가입 가능합니다',
+    SIGNUP_NOT_OK: '가입 불가능합니다',
+  
+    // 로그인
+    LOGIN_SUCCESS: '로그인 성공',
+    LOGIN_FAIL: '로그인 실패',
+    ALREADY_LOGIN: '이미 로그인 중입니다.',
+    NEED_LOGIN: '로그인이 필요한 서비스 입니다.',
+    NO_USER: '존재하지 않는 회원입니다.',
+    MISS_MATCH_PW: '비밀번호가 맞지 않습니다.',
+    INVALID_EMAIL: '이메일 형식을 확인해주세요.',
+
+    // 로그아웃
+    LOGOUT_SUCCESS: '로그아웃 성공',
+    LOGOUT_FAIL: '로그아웃 실패',
+
+    // 프로필 조회
+    READ_PROFILE_SUCCESS: '프로필 조회 성공',
+  
+    // 유저
+    READ_ONE_USER_SUCCESS: '유저 조회 성공',
+    READ_ALL_USERS_SUCCESS: '모든 유저 조회 성공',
+    UPDATE_ONE_USER_SUCCESS: '유저 수정 성공',
+    DELETE_ONE_USER_SUCCESS: '유저 삭제 성공',
+    DELETE_INFO_SUCCESS: '유저 정보 삭제 성공',
+    NICKNAME_EXIST: '닉네임이 중복되었습니다',
+    ACCESS_TOKEN_SUCCESS: 'access token 발급 성공',
+  
+    // 파일 업로드
+    ONLY_IMAGE_AVAILABLE: '이미지만 업로드 가능합니다',
+  
+    // 서버 내 오류
+    INTERNAL_SERVER_ERROR: '서버 내 오류',
+  
+    // 토큰
+    TOKEN_EXPIRED: '토큰이 만료되었습니다.',
+    TOKEN_INVALID: '토큰이 유효하지 않습니다.',
+    TOKEN_EMPTY: '토큰이 없습니다.',
+    NOT_TOKEN_EXPIRED: '토큰이 만료되지 않았습니다.',
+  
+    // 인증
+    NO_AUTH_HEADER: 'Authorization 헤더가 없습니다.',
+    NO_AUTH: '인증되지 않았습니다',
+  
+  };
