385d3eeedc44cf1e6f7bae8fdf12d286b6b0fd2c
senzoo-oh <osj3418@gmail.com>
2024-05-27 16:36:47 +0900

    feat: ✨회원 뷰 get 라우터 추가

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,39 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express"),
+  app = express(),
+  userController = require("../controllers/userController.js"),
+  router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
+/*
+로그인 관련 라우트
+*/
+router.get('/login', userController.login);     // 요청 url: localhost:3000/users/login
+
+
+
+
+/*
+회원가입 관련 라우트
+*/
+router.get('/join', userController.join);     // 요청 url: localhost:3000/users/join
+
+//router.post('/join', userController.create, userController.redirectView);
+
+
+
+
+
+/*
+비밀번호 변경 관련 라우트
+*/
+router.get('/password/emailVerification', userController.emailVerificationView);    // 요청 url: localhost:3000/users/password/emailVerification
+
+router.get('/password/change', userController.changePasswordView);    // 요청 url: localhost:3000/users/password/change
+
+//router.post('/password/emailVarificaiton', userController.changePassword);
+//router.post('/password/chagne', userController.changePassword);
+
+
+
 
 module.exports = router;
