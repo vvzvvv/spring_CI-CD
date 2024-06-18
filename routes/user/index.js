3790b9b294d0c106a9c8f89809986ff45bbc8b7e
senzoo-oh <osj3418@gmail.com>
2024-06-19 01:49:09 +0900

    refactor: 🔨이메일 인증, 비밀번호 변경 라우터 추가

--- routes/user/index.js
+++ routes/user/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const loginPOST = require("../../controllers/user/loginPOST");
 const joinPOST = require("../../controllers/user/joinPOST");
 const emailCheckingPOST = require("../../controllers/user/emailCheckingPOST");
+const findPassword = require("../../controllers/user/findPassword");
+const changePassword = require("../../controllers/user/changePassword");
 
 /*
-로그인 관련 라우트  요청 url: localhost:3000/users/login
+로그인 관련 라우트
 */
 router.get('/login', function(req, res, next) {
   res.render('user/login');
@@ -16,7 +18,7 @@ router.post('/login', loginPOST);
 
 
 /*
-회원가입 관련 라우트  요청 url: localhost:3000/users/join
+회원가입 관련 라우트
 */
 router.get('/join', function(req, res) {
   res.render("user/join");
@@ -29,18 +31,23 @@ router.post('/join/emailChecking', emailCheckingPOST);
 
 
 /*
-비밀번호 변경 관련 라우트
+이메일 인증 관련 라우트
 */
 router.get('/emailVerification', function(req, res) {
   res.render("user/emailVerification");
 });
 
-//router.get('/password/change', userController.changePasswordView);    // 요청 url: localhost:3000/users/password/change
-
-//router.post('/password/emailVarificaiton', userController.changePassword);
-//router.post('/password/chagne', userController.changePassword);
+router.post('/password/emailVarificaiton', findPassword);
 
 
+/*
+비밀번호 변경 관련 라우트
+*/
+router.get('/password/change', function(req, res) {
+  res.render("user/changePassword");
+});
+
+router.post('/password/change',changePassword);
 
 
 module.exports = router;
