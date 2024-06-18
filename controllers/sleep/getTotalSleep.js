ffcb0d6ca0bea3133844726b413434a0d0dced7f
senzoo-oh <osj3418@gmail.com>
2024-06-18 13:10:26 +0900

    feature: ✨수면기록 토큰 적용

--- controllers/sleep/getTotalSleep.js
+++ controllers/sleep/getTotalSleep.js
@@ -1,8 +1,13 @@
 const { getTotalSleepTime } = require('../../models/sleepQuery');
+const authenticateToken = require("../../authenticateToken");
 
 module.exports = async (req, res) => {
     try {
-        const userId = 1;
+
+        const authHeader = req.headers.authorization;
+        const token = authHeader.split(' ')[1];
+        const userId = await authenticateToken(token);
+
         const totalSleepTime = await getTotalSleepTime(userId);
         res.status(200).json(totalSleepTime);
     } catch (err) {
--- controllers/sleep/sleepByDateGET.js
+++ controllers/sleep/sleepByDateGET.js
@@ -1,10 +1,15 @@
 const { getSleepReportByDate } = require('../../models/sleepQuery');
+const authenticateToken = require("../../authenticateToken");
 
 module.exports = async (req, res) => {
     try {
-        const userId = 1;
         const {date} = req.params;
-        console.log("controller",date);
+
+        const authHeader = req.headers.authorization;
+        const token = authHeader.split(' ')[1];
+        const userId = await authenticateToken(token);
+
+        // console.log("controller",date);
         const sleepDataByDate = await getSleepReportByDate(date, userId) || [];
         
         res.status(200).json(sleepDataByDate);
