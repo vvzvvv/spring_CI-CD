e53452937b103d1bd667a9ffb729a83aa2ab2369
senzoo-oh <osj3418@gmail.com>
2024-06-19 04:44:33 +0900

    feature: ✨비밀번호 변경 기능 추가

--- routes/user/index.js
+++ routes/user/index.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const loginPOST = require("../../controllers/user/loginPOST");
 const joinPOST = require("../../controllers/user/joinPOST");
 const emailCheckingPOST = require("../../controllers/user/emailCheckingPOST");
-const findPassword = require("../../controllers/user/findPassword");
-const changePassword = require("../../controllers/user/changePassword");
+const passwordResetLinkPOST = require("../../controllers/user/passwordResetLinkPOST");
+const passwordPUT = require("../../controllers/user/passwordPUT");
 
 /*
 로그인 관련 라우트
@@ -37,17 +37,20 @@ router.get('/emailVerification', function(req, res) {
   res.render("user/emailVerification");
 });
 
-router.post('/password/emailVarificaiton', findPassword);
+router.post('/password/emailVarificaiton', passwordResetLinkPOST);
 
 
 /*
 비밀번호 변경 관련 라우트
 */
 router.get('/password/change', function(req, res) {
-  res.render("user/changePassword");
+  const {token} = req.query;
+  console.log('token: ', token);
+  
+  res.render("user/changePassword", {token});
 });
 
-router.post('/password/change',changePassword);
+router.put('/password/change', passwordPUT);
 
 
 module.exports = router;
