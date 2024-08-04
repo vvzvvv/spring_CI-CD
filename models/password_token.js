33cc18eedc46b2520277d6ae399602df714f50bd
senzoo-oh <osj3418@gmail.com>
2024-08-04 13:35:45 +0900

    database: 🗄️ 비밀번호 토큰 테이블명 수정

--- models/password_token.js
+++ models/password_token.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, Sequelize) => {
-    const PasswordToken = sequelize.define('passwordToken', {
+    const PasswordToken = sequelize.define('password_token', {
         email: {
             type: Sequelize.STRING,
             allowNull: false,
